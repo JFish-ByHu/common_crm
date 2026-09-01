34fbea47961ac70160ad4c9737fcb28e727c3256
rico.hu <rico.hu@quectel.com>
2026-09-01 09:44:15 +0800

    chore: update pnpm-workspace.yaml to allow builds for Prisma packages

--- crm_serve/src/app.module.ts
+++ crm_serve/src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { JwtModule } from '@nestjs/jwt'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { PrismaModule } from './prisma/prisma.module'
+import { AuthModule } from './module/iam/auth/auth.module'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.development', '.env']
+    }),
+    JwtModule.registerAsync({
+      global: true,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.getOrThrow<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: Number(configService.get<string>('JWT_EXPIRES_IN_SECONDS') ?? 604800)
+        }
+      })
+    }),
+    PrismaModule,
+    AuthModule
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
---
+++ crm_serve/src/common/result.ts
@@ -0,0 +1,21 @@
+import { StatusCode, type StatusDefinition } from './status-code'
+
+export class Result<T = unknown> {
+  constructor(
+    public readonly code: number,
+    public readonly data: T | null,
+    public readonly msg: string
+  ) {}
+
+  static success<T = unknown>(data: T | null = null, msg = StatusCode.SUCCESS.msg) {
+    return new Result(StatusCode.SUCCESS.code, data, msg)
+  }
+
+  static failure<T = unknown>(
+    status: StatusDefinition = StatusCode.INTERNAL_SERVER_ERROR,
+    data: T | null = null,
+    msg = status.msg
+  ) {
+    return new Result(status.code, data, msg)
+  }
+}
